b60746e80ed0e945f71d89c9db490d33bfabf533
稲畑 稔秋 <inahata@miharu.co.jp>
2019-08-19 15:36:59 +0900

    add rules

---
+++ ts/index.ts
@@ -0,0 +1,22 @@
+
+
+var f=function() {
+    return 1;
+};
+
+const j = 1 == 1
+
+const ks: Array<number> = [];
+
+//a
+const l = () => {
+    return 0;
+};
+
+
+/**
+
+*/
+export function test() {
+
+};
